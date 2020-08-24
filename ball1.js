2f052c0e710609127d921334cae77dc92df60f0d
CLEMONNT <66716611+CLEMONNT@users.noreply.github.com>
2020-08-24 20:51:58 +0530

    Add files via upload

---
+++ ball1.js
@@ -0,0 +1,23 @@
+class Ball{
+    constructor(x,y){
+    var options ={
+    restitution:0.3,
+    isStatic:false,
+    density:1.2,
+    friction:0.5
+}
+     this.body=Bodies.circle(x,y,20,options);
+     World.add(world, this.body);
+
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        ellipse(0,0,20,20);
+        pop();
+
+    }
+}
